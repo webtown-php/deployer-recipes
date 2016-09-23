3a010d578ba8faf1d53b14bcd93709be5b479bcd
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2016-09-23 15:39:19 +0200

    Add comments

--- recipes/kunstmaan.php
+++ recipes/kunstmaan.php
@@ -7,20 +7,20 @@ set('writable_dirs', ['app/cache', 'app/logs', 'web/uploads']);
 set('writable_use_sudo', false);
 
 /**
- * Erőszakosan töröljük a cache-t. Vmiért erre szükség van. Ha nem volt használva, akkor néha az assetic:dump elhasalt
- * hibaüzenettel.
+ * Sometimes we need a force cache cleaner
  */
 task('deploy:force-cache-clean', function() {
-    run ('rm -rf {{release_path}}/app/cache/prod');
+    run ('rm -rf {{release_path}}/app/cache/{{env}}');
 })->setPrivate();
-before('deploy:assetic:dump', 'deploy:force-cache-clean');
-after('deploy:assetic:dump', 'deploy:force-cache-clean');
+// Commented example
+//before('deploy:assetic:dump', 'deploy:force-cache-clean');
 
 task('deploy:gulp-build', function () {
     run('cd {{release_path}} && bundle install --path vendor/bundle');
     run('cd {{release_path}} && npm install');
     run('cd {{release_path}} && bower install');
     run('cd {{release_path}} && gulp build');
+    // clean after gulp
     run('rm -rf /tmp/gulp-ruby-sass/');
 })->desc('Install environment (Bundler, NPM, Bower)');
 after('deploy:assetic:dump', 'deploy:gulp-build');
