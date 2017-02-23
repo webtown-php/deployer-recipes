50086bbdade32769a9e07063a640f6bd4a0fe599
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2017-02-23 13:51:15 +0100

    Name bugfix

--- src/DependencyInjection/WebtownDeployerRecipesExtension.php
+++ src/DependencyInjection/WebtownDeployerRecipesExtension.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpKernel\DependencyInjection\Extension;
  *
  * To learn more see {@link http://symfony.com/doc/current/cookbook/bundles/extension.html}
  */
-class WebtownDeploymentExtension extends Extension
+class WebtownDeployerRecipesExtension extends Extension
 {
     /**
      * {@inheritdoc}
