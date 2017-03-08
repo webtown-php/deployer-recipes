76a64690bdef215adbcb7a85b6ebb07e9bc29c4d
Krisztián Ferenczi <ferenczi.krisztian@gmail.com>
2017-03-08 17:57:17 +0100

    SF2 compatibility fix

--- src/DependencyInjection/Compiler/TemplatesPass.php
+++ src/DependencyInjection/Compiler/TemplatesPass.php
@@ -3,8 +3,8 @@
 namespace Webtown\DeployerRecipesBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
-use Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Created by IntelliJ IDEA.
@@ -14,8 +14,6 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
  */
 class TemplatesPass implements CompilerPassInterface
 {
-    use PriorityTaggedServiceTrait;
-
     /**
      * You can modify the container here before it is dumped to PHP code.
      *
@@ -34,5 +32,42 @@ class TemplatesPass implements CompilerPassInterface
         }
 
         $container->getDefinition('webtown_deployer.template_manager')->addMethodCall('setTemplates', [$templates]);
+    }
+
+    /**
+     * Copy from SF3 for SF2 compatibility. (original: \Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait)
+     * @see \Symfony\Component\DependencyInjection\Compiler\PriorityTaggedServiceTrait
+     *
+     * Finds all services with the given tag name and order them by their priority.
+     *
+     * The order of additions must be respected for services having the same priority,
+     * and knowing that the \SplPriorityQueue class does not respect the FIFO method,
+     * we should not use this class.
+     *
+     * @see https://bugs.php.net/bug.php?id=53710
+     * @see https://bugs.php.net/bug.php?id=60926
+     *
+     * @param string           $tagName
+     * @param ContainerBuilder $container
+     *
+     * @return Reference[]
+     */
+    private function findAndSortTaggedServices($tagName, ContainerBuilder $container)
+    {
+        $services = array();
+
+        foreach ($container->findTaggedServiceIds($tagName) as $serviceId => $tags) {
+            foreach ($tags as $attributes) {
+                $priority = isset($attributes['priority']) ? $attributes['priority'] : 0;
+                $services[$priority][] = new Reference($serviceId);
+            }
+        }
+
+        if ($services) {
+            krsort($services);
+            $services = call_user_func_array('array_merge', $services);
+        }
+
+        return $services;
     }
 }
